bebefa0504fc55a695e233bb1754b1e5b0ab980e
Jose Vasquez <joseevb@protonmail.com>
2026-04-22 23:13:34 +0200

    refactor: format router configuration

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,7 @@
 import type { QueryClient } from '@tanstack/react-query'
 
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import {
-  HeadContent,
-  Scripts,
-  createRootRouteWithContext,
-} from '@tanstack/react-router'
+import { HeadContent, Scripts, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 
 import { ThemeProvider } from '@/components/providers/theme-provider'
